6b43613a72fe7ae26b873a850549e02d993c60c8
João Vitor J. <105255666+JaoVitorz@users.noreply.github.com>
2025-06-03 20:52:28 -0300

    adicionado imports no chat

--- pet-joyful/src/app/Chat/page.tsx
+++ pet-joyful/src/app/Chat/page.tsx
@@ -1,4 +1,9 @@
-import Chat from '../components/chat';
+import Chat from '../components/chat/chatlist';
+import Chat from '../components/chat/chat';
+import Chat from '../components/chat/chatinput';
+import Chat from '../components/chat/chatmessage';
+import Chat from '../components/chat/chatheader';
+
 
 export default function Home() {
   return <Chat />;
