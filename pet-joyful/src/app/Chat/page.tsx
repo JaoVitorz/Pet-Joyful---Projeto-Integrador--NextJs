1d6537baf10765fc221c133a900352dd4ed9eff2
João Vitor J. <105255666+JaoVitorz@users.noreply.github.com>
2025-06-03 21:37:31 -0300

    adicionado header no chat

--- pet-joyful/src/app/Chat/page.tsx
+++ pet-joyful/src/app/Chat/page.tsx
@@ -1,7 +1,11 @@
 import Chat from "../components/chat/chat";
+import Header from "../components/common/Header";
 
 export default function ChatPage() {
-    
-  return <Header/>, 
-  <Chat />;
+  return (
+    <>
+      <Header />
+      <Chat />
+    </>
+  );
 }
