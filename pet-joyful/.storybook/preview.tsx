9f1d4c87eb2c3626d831f83d339a92810f13f93d
Nicollas Mencacci <nicollasmencaccipereira@gmail.com>
2026-05-21 17:22:24 -0300

    chore(storybook): adiciona e configura o Storybook no projeto

---
+++ pet-joyful/.storybook/preview.tsx
@@ -0,0 +1,35 @@
+import type { Preview } from '@storybook/nextjs-vite'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/app/globals.css';
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props: any) => {
+    return <OriginalNextImage {...props} unoptimized />;
+  },
+});
+
+
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
+  },
+};
+
+export default preview;
